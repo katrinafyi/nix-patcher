7df3e4d172bba939b3e1ca4f259b56efc8e3b231
rina <k@rina.fyi>
2026-04-14 01:11:51 +1000

    packages.default

--- flake.nix
+++ flake.nix
@@ -3,28 +3,19 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       systems = nixpkgs.legacyPackages.x86_64-linux.go.meta.platforms;
       lib = nixpkgs.lib;
     in
     {
-      packages = lib.genAttrs systems (sys: 
-        let 
+      packages = lib.genAttrs systems (sys:
+        let
           pkgs = nixpkgs.legacyPackages.${sys};
         in
         {
-          nix-patcher = pkgs.callPackage ./patcher.nix {
-            nix = pkgs.nixVersions.nix_2_18; # "nix flake update" arguments changed...
-          };
-        }
-      );
-
-      apps = lib.genAttrs systems (sys: 
-        let selfpkgs = self.packages.${sys};
-        in {
-          default.type = "app";
-          default.program = lib.getExe selfpkgs.nix-patcher;
+          default = self.packages.${sys}.nix-patcher;
+          nix-patcher = pkgs.callPackage ./patcher.nix { };
         }
       );
     };
